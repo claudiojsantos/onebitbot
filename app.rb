f1638ae34268fa246db9723d1c1bbd6be7b9317c
Claudio Santos <cljos@hotmail.com>
2020-09-03 22:38:30 -0300

    chore: create project, add gems, create connection, dockerize project and migration/migrate

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'json'
+require 'sinatra'
+require 'sinatra/activerecord'
+
+require './config/database'
+
+Dir["./app/models/*.rb"].each { |file| require file }
+
+class App < Sinatra::Base
+    get '/' do
+        'Hello World'
+    end
+end
